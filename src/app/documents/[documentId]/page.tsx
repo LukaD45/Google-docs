20f19d4c5e42a65a18d89157ab5024e8b1e95b1d
LukaD45 <luka.dusko44@gmail.com>
2024-12-10 07:12:31 +0100

    Undo button inside toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,12 +1,15 @@
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
   return (
     <div className="min-h-screen bg-[#fafbfd]">
+      <Toolbar />
       <Editor />
     </div>
   );
