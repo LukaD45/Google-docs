b71eb69944b00762ad50cd307d805bb97519f79f
LukaD45 <luka.dusko44@gmail.com>
2024-12-12 23:48:28 +0100

    'File' submenu prepared

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,6 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import { FileIcon } from "lucide-react";
+import {
+  FileIcon,
+  FileJsonIcon,
+  FilePenIcon,
+  FilePlusIcon,
+  FileText,
+  GlobeIcon,
+  PrinterIcon,
+  TrashIcon,
+} from "lucide-react";
 
 import {
   Menubar,
@@ -16,6 +27,7 @@ import {
 } from "@/components/ui/menubar";
 
 import { DocumentInput } from "./document-input";
+import { BsFilePdf } from "react-icons/bs";
 
 export const Navbar = () => {
   return (
@@ -29,13 +41,68 @@ export const Navbar = () => {
           <div className="flex">
             <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
               <MenubarMenu>
-                <MenubarTrigger>File</MenubarTrigger>
-                <MenubarContent>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  File
+                </MenubarTrigger>
+                <MenubarContent className="print:hidden">
+                  <MenubarSub>
+                    <MenubarSubTrigger>
+                      <FileIcon className="size-4 mr-2" />
+                      Save
+                    </MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>
+                        <FileJsonIcon className="size-4 mr-2" />
+                        JSON
+                      </MenubarItem>
+                      <MenubarItem>
+                        <GlobeIcon className="size-4 mr-2" />
+                        HTML
+                      </MenubarItem>
+                      <MenubarItem>
+                        <BsFilePdf className="size-4 mr-2" />
+                        PDF
+                      </MenubarItem>
+                      <MenubarItem>
+                        <FileText className="size-4 mr-2" />
+                        Text
+                      </MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
                   <MenubarItem>
-                    <FileIcon className="size-4 mr-2" />
-                    Save
+                    <FilePlusIcon className="size-4 mr-2" />
+                    New document
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem>
+                    <FilePenIcon className="size-4 mr-2" />
+                    Rename
+                  </MenubarItem>
+                  <MenubarItem>
+                    <TrashIcon className="size-4 mr-2" />
+                    Remove
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem onClick={() => window.print()}>
+                    <PrinterIcon className="size-4 mr-2" />
+                    Print <MenubarShortcut>Ctrl + P</MenubarShortcut>
                   </MenubarItem>
                 </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Edit
+                </MenubarTrigger>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Insert
+                </MenubarTrigger>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                  Format
+                </MenubarTrigger>
               </MenubarMenu>
             </Menubar>
           </div>
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -10,9 +10,13 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
   return (
     <div className="min-h-screen bg-[#fafbfd]">
-      <Navbar />
-      <Toolbar />
-      <Editor />
+      <div className="flex flex-col px.4 gap-y-2 fixed top-0 left-0 right-0 z-[10] bg-[#fafbfd] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 };
