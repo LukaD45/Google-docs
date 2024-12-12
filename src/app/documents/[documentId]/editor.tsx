a922c708699b967c73231a264d68fcf2b8a4cc0d
LukaD45 <luka.dusko44@gmail.com>
2024-12-12 20:37:46 +0100

    immediatelyRender set as false bc of possible hydration error

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,10 +20,12 @@ import ImageResize from "tiptap-extension-resize-image";
 import { useEditorStore } from "@/store/use-editor-store";
 import Underline from "@tiptap/extension-underline";
 import TextStyle from "@tiptap/extension-text-style";
+import { Ruler } from "./ruler";
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -106,6 +108,7 @@ export const Editor = () => {
   });
   return (
     <div className="size-full overflow-x-auto bg-[#f9fbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         {" "}
         <EditorContent editor={editor} />
