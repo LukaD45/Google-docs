56a9ae74c52407fdd7d94b46d0d78c0415414157
LukaD45 <luka.dusko44@gmail.com>
2024-12-13 21:57:37 +0100

    implemented Undo functionality onClick

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -36,8 +36,10 @@ import {
 
 import { DocumentInput } from "./document-input";
 import { BsFilePdf } from "react-icons/bs";
+import { useEditorStore } from "@/store/use-editor-store";
 
 export const Navbar = () => {
+  const { editor } = useEditorStore();
   return (
     <nav className="flex items-center justify-between">
       <div className="flex gap-2 items-center">
@@ -102,7 +104,9 @@ export const Navbar = () => {
                   Edit
                 </MenubarTrigger>
                 <MenubarContent>
-                  <MenubarItem>
+                  <MenubarItem
+                    onClick={() => editor?.chain().focus().undo().run()}
+                  >
                     <Undo2Icon className="size-4 mr-2" />
                     Undo<MenubarShortcut>Ctrl + Z</MenubarShortcut>
                   </MenubarItem>
