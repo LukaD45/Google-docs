6d5ac5b0bdcda97391945f59367df5614c56db3d
LukaD45 <luka.dusko44@gmail.com>
2024-12-19 09:33:28 +0100

    created documents-table component

---
+++ src/app/(home)/documents-table.tsx
@@ -0,0 +1,17 @@
+import { PaginationStatus } from "convex/react";
+
+import { Doc } from "../../../convex/_generated/dataModel";
+
+interface DocumentsTableProps {
+  documents: Doc<"documents">[] | undefined;
+  loadMore: (numItems: number) => void;
+  status: PaginationStatus;
+}
+
+export const DocumentsTable = ({
+  documents,
+  loadMore,
+  status,
+}: DocumentsTableProps) => {
+  return <div>Doc table</div>;
+};
