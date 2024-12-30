696470fc19f706b44e4d364e5a97e5c4be53b701
LukaD45 <luka.dusko44@gmail.com>
2024-12-30 15:54:18 +0100

    inbox component developed

---
+++ src/app/documents/[documentId]/inbox.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { BellIcon } from "lucide-react";
+import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui";
+import { useInboxNotifications } from "@liveblocks/react/suspense";
+import { ClientSideSuspense } from "@liveblocks/react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+
+export const Inbox = () => {
+  return (
+    <ClientSideSuspense fallback={null}>
+      <InboxMenu />
+    </ClientSideSuspense>
+  );
+};
+
+const InboxMenu = () => {
+  const { inboxNotifications } = useInboxNotifications();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative" size="icon">
+          <BellIcon className="size-5 " />
+          {inboxNotifications?.length > 0 && (
+            <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white justify-center items-center">
+              {inboxNotifications.length}
+            </span>
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-auto">
+        {inboxNotifications.length > 0 ? (
+          <InboxNotificationList></InboxNotificationList>
+        ) : (
+          <div className="p-2 w-[400px] text-center text-sm text-muted-foreground">
+            No notifications
+          </div>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
