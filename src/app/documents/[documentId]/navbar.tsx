653b3ce596adb1b798f4ea44f2c4a7c24a73da03
LukaD45 <luka.dusko44@gmail.com>
2024-12-30 15:44:41 +0100

    fixed avatars component

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -36,6 +36,7 @@ import {
 
 import { DocumentInput } from "./document-input";
 import { BsFilePdf } from "react-icons/bs";
+import { Avatars } from "./avatars";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
@@ -259,6 +260,7 @@ export const Navbar = () => {
         </div>
       </div>
       <div className="flex gap-3 items-center pl-6">
+        <Avatars />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
