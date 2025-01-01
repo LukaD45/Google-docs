b3297b08903bc904bb064d92420a0a640ecea6b2
LukaD45 <luka.dusko44@gmail.com>
2025-01-01 15:13:39 +0100

    connection status icon

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -1,8 +1,12 @@
-import { BsCloudCheck } from "react-icons/bs";
+import { BsCloudCheck, BsCloudSlash } from "react-icons/bs";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
+import { useDebounce } from "@/hooks/use-debounce";
+import { toast } from "sonner";
+import { useStatus } from "@liveblocks/react";
+import { LoaderIcon } from "lucide-react";
 
 interface DocumentInputProps {
   title: string;
@@ -10,8 +14,10 @@ interface DocumentInputProps {
 }
 
 export const DocumentInput = ({ title, id }: DocumentInputProps) => {
+  const status = useStatus();
+
   const [value, setValue] = useState(title);
-  const [isError, setIsError] = useState(false);
+
   const [isPending, setIsPending] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -19,15 +25,43 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
 
   const mutate = useMutation(api.documents.updateById);
 
+  const debouncedUpdate = useDebounce((newValue: string) => {
+    if (newValue === value) return;
+
+    setIsPending(true);
+    mutate({ id, title: newValue })
+      .then(() => toast.success("Document updated"))
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
+  });
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
     setValue(newValue);
+    debouncedUpdate(newValue);
   };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsPending(true);
+    mutate({ id, title: value })
+      .then(() => {
+        toast.success("Document updated");
+        setIsEditing(false);
+      })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
+  };
+
+  const showLoader =
+    isPending || status === "connecting" || status === "reconnecting";
+  const showError = status === "disconnected";
 
   return (
     <div className="flex items-center gap-2">
       {isEditing ? (
-        <form className="relative w-fit mx-w-[50ch]">
+        <form onSubmit={handleSubmit} className="relative w-fit mx-w-[50ch]">
           <span className="invisible whitespace-pre px-1.5 text-lg">
             {value || ""}
           </span>
@@ -35,6 +69,7 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
             ref={inputRef}
             value={value}
             onChange={onChange}
+            onBlur={() => setIsEditing(false)}
             className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"
           />
         </form>
@@ -51,8 +86,11 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
           {title}
         </span>
       )}
-
-      <BsCloudCheck />
+      {showError && <BsCloudSlash className="size-4" />}
+      {!showError && !showLoader && <BsCloudCheck className="size-4" />}
+      {showLoader && (
+        <LoaderIcon className="size-4 animate-spin text-muted-foreground" />
+      )}
     </div>
   );
 };
