e7ddc2b8d7d928fd358ec57fc77dc62f622ddab7
LukaD45 <luka.dusko44@gmail.com>
2025-01-01 12:30:41 +0100

    notifications functionality fully completed

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { ReactNode, useEffect, useMemo, useState } from "react";
+import { useParams } from "next/navigation";
+import { toast } from "sonner";
+
 import {
   LiveblocksProvider,
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
-import { useParams } from "next/navigation";
+import { getUsers, getDocuments } from "./actions";
+
 import { FullscreenLoader } from "@/components/fullscreen-loader";
-import { getUsers } from "./actions";
-import { toast } from "sonner";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 type User = { id: string; name: string; avatar: string };
 
@@ -63,7 +66,13 @@ export function Room({ children }: { children: ReactNode }) {
         }
         return filteredUsers.map((user) => user.id);
       }}
-      resolveRoomsInfo={() => []}
+      resolveRoomsInfo={async ({ roomIds }) => {
+        const documents = await getDocuments(roomIds as Id<"documents">[]);
+        return documents.map((document) => ({
+          id: document.id,
+          name: document.name,
+        }));
+      }}
     >
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense
